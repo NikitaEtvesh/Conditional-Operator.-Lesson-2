a2eea70957c9354c5ce8940a5add07735a74e5d0
Nikita <nikitaetvesh@yandex.ru>
2025-06-19 08:56:02 +0300

    Conditional Operator. Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int theOperatingSystem = 0;
+        if (theOperatingSystem == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (theOperatingSystem == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Задача 2
+        System.out.println("\nЗадача 2");
+        theOperatingSystem = 1;
+        int clientDeviceYear = 2015;
+        if (theOperatingSystem == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (theOperatingSystem == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (theOperatingSystem == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (theOperatingSystem == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        }
+        // Задача 3
+        System.out.println("\nЗадача 3");
 
     }
 }
