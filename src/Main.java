6e304a892c6fd9bfd06e1b2f92d99354f03fb64c
Nikita <nikitaetvesh@yandex.ru>
2025-06-19 10:39:08 +0300

    Conditional Operator. Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         } else if (theOperatingSystem == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
         // Задача 2
         System.out.println("\nЗадача 2");
         theOperatingSystem = 1;
@@ -21,8 +22,72 @@ public class Main {
         } else if (theOperatingSystem == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
+
         // Задача 3
         System.out.println("\nЗадача 3");
+        int year = 2024;
+        if (year <= 1584) {
+            System.out.println("Год должен быть больше 1584");
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
+        // Задача 4
+        System.out.println("\nЗадача 4");
+        int deliveryDistance = 50;
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+        System.out.println("Потребуется дней: " + deliveryDays);
+
+        // Задача 5
+        System.out.println("\nЗадача 5");
+        int monthNumber = 12;
+        String season = getSeason(monthNumber);
+        System.out.println("Сезон: " + season);
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+
+    public static String getSeason(int monthNumber) {
+        String season;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                season = "Зима";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Весна";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Лето";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Осень";
+                break;
+            default:
+                season = "Неверный номер месяца";
+                break;
+        }
+
+        return season;
     }
 }
